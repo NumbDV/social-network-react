983e887cbd2cee1984f8303f273304407c70ad83
Denys <d3n.vor@gmail.com>
2020-02-03 15:51:03 +0200

    #14 added CSS modules, styling some elements

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,25 @@
 import React from 'react';
+import classes from './Navbar.module.css';
+console.log(classes);
+
+let activeItem = `${classes.item} ${classes.active}`;
 
 const Navbar = () => {
     return (
-        <nav className='nav'>
-            <div>
+        <nav className={classes.nav}>
+            <div className={classes.item}>
                 <a>Profile</a>
             </div>
-            <div>
+            <div className={activeItem}>
                 <a>Messages</a>
             </div>
-            <div>
+            <div className={classes.item}>
                 <a>News</a>
             </div>
-            <div>
+            <div className={classes.item}>
                 <a>Music</a>
             </div>
-            <div>
+            <div className={classes.item}>
                 <a>Settings</a>
             </div>
         </nav>
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,25 +1,26 @@
 import React from 'react';
+import classes from './Profile.module.css';
 
 const Profile = () => {
     return (
-        <div className='content'>
+        <div className={classes.content}>
             <div>
                 <img src='https://images.unsplash.com/photo-1499084732479-de2c02d45fcc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80' />
             </div>
             <div>
                 ava + descr
-				</div>
-            <div>
+			</div>
+            <div className={classes.posts}>
                 my posts
-					<div>
+				<div>
                     new post
-					</div>
-                <div>
+				</div>
+                <div className={classes.item}>
                     post 1
-					</div>
-                <div>
+				</div>
+                <div className={classes.item}>
                     post 2
-					</div>
+				</div>
             </div>
         </div>
     );
