db52371bdb5ccfde9005d296cd848ec25b520a6a
Denys <d3n.vor@gmail.com>
2020-02-04 14:36:43 +0200

    #20 started making dialogs sections, added some styles

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,26 @@ import styles from './Dialogs.module.css';
 
 const Dialogs = (props) => {
     return (
-        <div>
-            Dialogs
+        <div className={styles.dialogs}>
+            <div className={styles.dialogsItems}>
+                <div className={styles.dialog + ' ' + styles.active}>
+                    Den
+                </div>
+                <div className={styles.dialog}>
+                    Valera
+                </div>
+                <div className={styles.dialog}>
+                    Sveta
+                </div>
+                <div className={styles.dialog}>
+                    Marta
+                </div>
+            </div>
+            <div className={styles.messages}>
+                <div className={styles.message}>Hop</div>
+                <div className={styles.message}>Hey</div>
+                <div className={styles.message}>Lalaley</div>
+            </div>
         </div>
     );
 }
